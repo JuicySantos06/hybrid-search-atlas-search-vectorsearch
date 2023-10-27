7dc6183b42e7887d8fcdc5354d5359e9921750ee
JuicySantos06 <84564830+JuicySantos06@users.noreply.github.com>
2023-10-27 13:54:37 +0200

    Update hybrid_search_text_search_and_vector_search.py

--- hybrid_search_text_search_and_vector_search.py
+++ hybrid_search_text_search_and_vector_search.py
@@ -7,31 +7,26 @@ import requests
 import time
 import numpy
 
-#define global variables
 mongodbAtlasUri = "mongodb+srv://akilsk:Sofia06061991.@cluster0.vdddl.mongodb.net/?retryWrites=true&w=majority"
 mongodbAtlasDatabase = "hybrid_search_xmarket"
 mongodbAtlasCollection = "hybrid_search_dataset"
 userQuery = "ice cream spon"
 numOfResults = 10
 
-#define html result file initialization - https://towardsdatascience.com/text-search-vs-vector-search-better-together-3bd48eb6132a
 def init_result_file_html(paramQuery):
     textFile = open("./hybridSearchResult.html","w")
     textFile.write("<html>\n<head>\n<title> \nMongoDB Hybrid Search Results \</title>\n</head><body><h><center>User Search Query: "+paramQuery+"</center></h><hr></body>\n")
     textFile.close()
 
-#enrich html report file
 def insert_data_result_file(paramHtmlFileResult,paramSectionTitle):
     textFile = open("./hybridSearchResult.html","a")
     textFile.write("\n<body><h1><center>"+paramSectionTitle+"</center></h1>\n</body>")
     textFile.write("\n<body><i1><center>"+paramHtmlFileResult+"</center></i1>\n</body>")
     textFile.write("\n<body><j1></j1>\n<hr></body>\n")
     textFile.close()
-
-#define model to be used for encoding product title field
+    
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     
-#define db connection initialization
 def startup_db_connection(paramMongodbAtlasConnectionString):
     try:
         mongodbClient = MongoClient(paramMongodbAtlasConnectionString)
@@ -40,13 +35,11 @@ def startup_db_connection(paramMongodbAtlasConnectionString):
     except pymongo.errors.OperationFailure as err:
         print (f"Datacase Connection failed. Error: {err}")
 
-#define db client initialization
 def startup_db_client(paramStartupDbConnection,paramMongodbDatabaseName):
     if (paramStartupDbConnection):
         mongodbDatabase = paramStartupDbConnection[paramMongodbDatabaseName]
         return mongodbDatabase
 
-#Atlas Search query
 def mongodb_atlas_search_query(paramStartupDbClient,paramMongodbCollectionName,paramUserQuery,paramNumOfResults):
     mongodbCollection = paramStartupDbClient[paramMongodbCollectionName]
     try:
@@ -82,7 +75,6 @@ def mongodb_atlas_search_query(paramStartupDbClient,paramMongodbCollectionName,p
     except OperationFailure as err:
         print (f"Error related to mongodb_atlas_search_query function: {err}")
 
-#product image retrieval
 def mongodb_atlas_product_img_retrieval(paramStartupDbClient,paramMongodbCollectionName,paramProductId):
     mongodbCollection = paramStartupDbClient[paramMongodbCollectionName]
     try:
@@ -106,7 +98,6 @@ def mongodb_atlas_product_img_retrieval(paramStartupDbClient,paramMongodbCollect
     except OperationFailure as err:
         print(f"Error related to mongodb_atlas_product_img_retrieval function: {err}")
 
-#define Atlas Vector Search query
 def mongodb_atlas_vector_search_query(paramStartupDbClient,paramMongodbCollectionName,queryEmbedding,paramNumOfResults):
     mongodbCollection = paramStartupDbClient[paramMongodbCollectionName]
     try:
@@ -134,7 +125,6 @@ def mongodb_atlas_vector_search_query(paramStartupDbClient,paramMongodbCollectio
         return vectorSearchResult
     except OperationFailure as err:
         print (f"Error related to mongodb_atlas_vector_search_query function: {err}")
-
 
 def mongodb_atlas_cleanse_enrich(paramStartupDbClient,paramMongodbCollectionName):
     mongodbCollection = paramStartupDbClient[paramMongodbCollectionName]
@@ -188,12 +178,9 @@ def mongodb_atlas_cleanse_enrich(paramStartupDbClient,paramMongodbCollectionName
     except OperationFailure as err:
         print (f"Error related to mongodb_atlas_cleanse_enrich function: {err}")
     
-
-# convert your links to html tags 
 def download_product_image(paramUrl,paramFileName):
     url = paramUrl
     file_name = "./" + paramFileName
-
     res = requests.get(url, stream = True).content
     with open(file_name,'wb') as f:
         f.write(res)
@@ -206,16 +193,12 @@ def to_img_tag(path):
 def main():
     init_result_file_html(userQuery)
     listOfHtmlFileResults = dict()
-
     currMongoClient = startup_db_connection(mongodbAtlasUri)
-
     pandas.set_option('mode.chained_assignment',None)
 
     #ATLAS SEARCH
     searchResultList = mongodb_atlas_search_query(startup_db_client(currMongoClient,mongodbAtlasDatabase),mongodbAtlasCollection,userQuery,numOfResults)
     searchResultDataFrame = pandas.DataFrame(list(searchResultList))
-    #print(searchResultDataFrame)
-
     searchResultMaxScore = searchResultDataFrame['score'].loc[searchResultDataFrame.index[0]]
     searchResultNumpyArray = searchResultDataFrame.to_numpy()
     
@@ -232,7 +215,6 @@ def main():
         for doc in mongodb_atlas_product_img_retrieval(startup_db_client(currMongoClient,mongodbAtlasDatabase),mongodbAtlasCollection,productId):
             dictOfProductImg.append(doc['imgUrl'][0].split('"')[1])  
     displaySearchResult['IMAGE'] = dictOfProductImg
-
     listOfHtmlFileResults["MongoDB Atlas Search Results"] = displaySearchResult.to_html(escape=False,formatters=dict(IMAGE=to_img_tag))
 
     #WRITE ATLAS SEARCH RESULT INTO MONGODB COLLECTION
@@ -254,9 +236,6 @@ def main():
     #ATLAS VECTOR SEARCH
     userQueryVectorEmbedding = model.encode(userQuery)
     vectorSearchResultList = mongodb_atlas_vector_search_query(startup_db_client(currMongoClient,mongodbAtlasDatabase),mongodbAtlasCollection,userQueryVectorEmbedding.tolist(),numOfResults)
-    
-    #print(vectorSearchResultList)
-
     vectorSearchResultDataFrame = pandas.DataFrame(list(vectorSearchResultList))
     vectorSearchResultMaxScore = vectorSearchResultDataFrame['score'].loc[searchResultDataFrame.index[0]]
     vectorSearchResultNumpyArray = vectorSearchResultDataFrame.to_numpy()
@@ -274,7 +253,6 @@ def main():
         for doc in mongodb_atlas_product_img_retrieval(startup_db_client(currMongoClient,mongodbAtlasDatabase),mongodbAtlasCollection,productId):
             dictOfProductImg.append(doc['imgUrl'][0].split('"')[1])  
     displayVectorSearchResult['IMAGE'] = dictOfProductImg
-
     listOfHtmlFileResults["MongoDB Atlas Vector Search Results"] = displayVectorSearchResult.to_html(escape=False,formatters=dict(IMAGE=to_img_tag))
 
     #WRITE ATLAS VECTOR SEARCH RESULT INTO MONGODB COLLECTION
@@ -293,17 +271,11 @@ def main():
         except OperationFailure as err:
             print(f"Error related to mongodb_atlas_product_img_retrieval function: {err}")
 
-
-    #CLEANSE AND ENRICH PREVIOUS RESULTS
-    time.sleep(2)
     mongodb_atlas_cleanse_enrich(startup_db_client(currMongoClient,mongodbAtlasDatabase),"atlasSearchQueryResult")
-    #CLEANSE AND ENRICH PREVIOUS RESULTS
-    time.sleep(2)
     mongodb_atlas_cleanse_enrich(startup_db_client(currMongoClient,mongodbAtlasDatabase),"atlasVectorSearchQueryResult")
 
     for key,value in listOfHtmlFileResults.items():
         insert_data_result_file(value,key)
 
 if __name__ == "__main__":
-
     main()
